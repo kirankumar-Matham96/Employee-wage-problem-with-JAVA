edc84d1e42c1e405f853251d6ee636a941894b3d
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-22 12:38:46 +0530

    ADD: Employee monthly wage is calculated

--- employee_wage.java
+++ employee_wage.java
@@ -7,27 +7,31 @@ public class employee_wage
 		int wagePerHr = 20;
 		int fullTimeWorkingHrs = 8;
 		int partTimeWorkingHrs = 4;
-		int empWage = 0;
-		int checkPresence = (int) (Math.random()*10%2);
-		int checkWorkingTime = (int) (Math.random()*10%2);
-		switch(checkPresence)
+		int workingDaysInMonth = 20;
+		int dayOfMonth = 0;
+		int empDailyWage = 0;
+		int empMonthlyWage = 0;
+		while(dayOfMonth < workingDaysInMonth)
 		{
-			case 1:
-				System.out.println("Employee is present");
-				switch(checkWorkingTime)
-				{
-					case 1:
-						System.out.println("Employee is full timer");
-						empWage = fullTimeWorkingHrs * wagePerHr;
-						break;
-					default:
-						System.out.println("Employee is part timer");
-						empWage = partTimeWorkingHrs * wagePerHr;
-				}
-				break;
-			default:
-				System.out.println("Employee is absent");
+			int checkPresence = (int) (Math.random()*10%2);
+			int checkWorkingTime = (int) (Math.random()*10%2);
+			switch(checkPresence)
+			{
+				case 1:
+					switch(checkWorkingTime)
+					{
+						case 1:
+							empDailyWage = fullTimeWorkingHrs * wagePerHr;
+							break;
+						default:
+							empDailyWage = partTimeWorkingHrs * wagePerHr;
+					}
+					empMonthlyWage += empDailyWage;
+					break;
+				default:
+			}
+			dayOfMonth++;
 		}
-		System.out.println("Employee Wage is: "+empWage);
+		System.out.println("Employee monthly wage is: "+empMonthlyWage);
 	}
 }
