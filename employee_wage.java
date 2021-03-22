2d9b3ea728b6209c9ac31afc6cc1a9ae9eb201a4
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-22 12:52:22 +0530

    ADD: Employee wage calculated for a month until max hrs or max days reached

--- employee_wage.java
+++ employee_wage.java
@@ -8,10 +8,12 @@ public class employee_wage
 		int fullTimeWorkingHrs = 8;
 		int partTimeWorkingHrs = 4;
 		int workingDaysInMonth = 20;
+		int maxWorkingHrs = 100;
 		int dayOfMonth = 0;
 		int empDailyWage = 0;
 		int empMonthlyWage = 0;
-		while(dayOfMonth < workingDaysInMonth)
+		int totalWorkingHrs = 0;
+		while(dayOfMonth < workingDaysInMonth || totalWorkingHrs < maxWorkingHrs)
 		{
 			int checkPresence = (int) (Math.random()*10%2);
 			int checkWorkingTime = (int) (Math.random()*10%2);
@@ -22,9 +24,11 @@ public class employee_wage
 					{
 						case 1:
 							empDailyWage = fullTimeWorkingHrs * wagePerHr;
+							totalWorkingHrs += fullTimeWorkingHrs;
 							break;
 						default:
 							empDailyWage = partTimeWorkingHrs * wagePerHr;
+							totalWorkingHrs += partTimeWorkingHrs;
 					}
 					empMonthlyWage += empDailyWage;
 					break;
