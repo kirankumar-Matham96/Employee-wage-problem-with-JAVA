4f72af270b29d2ac90cbe8a891d8affd4a965e33
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-22 10:53:23 +0530

    ADD: Checking Employee Presence

--- employee_wage.java
+++ employee_wage.java
@@ -2,6 +2,15 @@ public class employee_wage
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to employee_wage problem!");
+		int isPresent = 1;
+		int checkPresence = (int) Math.floor(Math.random()*10%2);
+		if(isPresent == checkPresence)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}
 }
